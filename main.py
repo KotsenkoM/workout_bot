57f7a5999b9b432ec5ccc035005ae4858d470772
KotsenkoM <maksimkcenk93@gmail.com>
2023-04-07 17:00:52 +0300

    add sheduler

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import asyncio
 import logging
 import os
 
 from aiogram import Bot, Dispatcher, executor
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,16 +13,16 @@ logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+scheduler = AsyncIOScheduler()
 
 
 async def start_poll():
-    question = 'Го на турнички'
-    options = ['го', 'нет']
-    while True:
-        await bot.send_poll(chat_id=CHAT_ID, question=question, options=options, is_anonymous=False)
-        await asyncio.sleep(5)
+    question = 'Го на турнички через 5 минут?'
+    options = ['Иду', 'Пропускаю']
+    await bot.send_poll(chat_id=CHAT_ID, question=question, options=options, is_anonymous=False)
 
 
 if __name__ == '__main__':
-    asyncio.get_event_loop().create_task(start_poll())
+    scheduler.add_job(start_poll, 'cron', day_of_week='mon-fri', hour='10, 15', minute=55)
+    scheduler.start()
     executor.start_polling(dp, skip_updates=True)
