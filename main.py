47b4a2a1249bfd33f63f99997a8752e5f943d1e4
KotsenkoM <maksimkcenk93@gmail.com>
2023-12-26 15:07:23 +0300

    Добавлен опросник для супа и хинкалей. Настроен пайп для сборки прода через гитлаб

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import logging
 import os
 
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -24,20 +25,40 @@ async def start_poll_workout():
     await bot.send_poll(chat_id=CHAT_ID, question='Го на турнички через 5 минут?', options=answer, is_anonymous=False)
 
 
-# @dp.message_handler(commands=['what'])
-# async def start_command(message: types.Message):
-#     # Получите ID группового чата
-#     chat_id = message.chat.id
-#     await message.reply(f"ID группового чата: {chat_id}")
+async def start_poll_soup():
+    """
+    Создает голосование в телеграм чате
+    """
+    answer = ['Иду', 'Пас']
+    now = datetime.now()
+    # Получаем день недели (понедельник - 0, воскресенье - 6)
+    day_of_week = now.weekday()
+
+    soup_dict = {
+        0: "Борщч",
+        1: "Горох",
+        2: "Лапша",
+        3: "ХИНКАЛИ! Ну а в кофепорте опять борщч",
+        4: "Рассольник"
+    }
+    await bot.send_poll(
+        chat_id=CHAT_ID,
+        question=f'Го за супчиком через 5 минут? Сегодня {soup_dict[day_of_week]}',
+        options=answer,
+        is_anonymous=False
+    )
 
 
-# async def pdr_search():
-#     await bot.send_message(chat_id=CHAT_ID, text='/pidoreg@SublimeBot')
-
-
+async def start_poll_khinkali():
+    """
+    Создает голосование в телеграм чате
+    """
+    answer = ['Го в 12:30', 'Го в 13:00', 'Го в 13:30', 'Пас']
+    await bot.send_poll(chat_id=CHAT_ID, question='Го в хинкальную сегодня?', options=answer, is_anonymous=False)
 
 if __name__ == '__main__':
-    scheduler.add_job(start_poll_workout, 'cron', day_of_week='mon-fri', hour='10, 15', minute='55')
-    # scheduler.add_job(pdr_search, 'cron', hour='15', minute='34')
+    # scheduler.add_job(start_poll_workout, 'cron', day_of_week='mon-fri', hour='10, 15', minute='55')
+    scheduler.add_job(start_poll_soup, 'cron', day_of_week='mon-fri', hour='10', minute='50')
+    scheduler.add_job(start_poll_khinkali, 'cron', day_of_week='thu', hour='10', minute='45')
     scheduler.start()
     executor.start_polling(dp, skip_updates=True)
